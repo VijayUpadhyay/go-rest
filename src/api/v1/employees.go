a65f84124d58027c18891bc71e012d1e6ba4eb91
Vijay Upadhyay <vijay.upadhyay@connectwise.com>
2023-05-08 22:52:57 +0530

    added retry

--- src/api/v1/employees.go
+++ src/api/v1/employees.go
@@ -1,11 +1,35 @@
 package v1
 
-import "github.com/sirupsen/logrus"
+import (
+	"github.com/avast/retry-go"
+	"github.com/cenkalti/backoff"
+	"github.com/sirupsen/logrus"
+)
 
 func GetEmployee() {
 	logrus.Debugf("Inside GetEmployee")
+
 }
 
 func CreateEmployee() {
 	logrus.Debugf("Inside CreateEmployee")
+}
+
+func BackOffOnError() {
+	// An operation that may fail.
+	operation := func() error {
+		return nil // or an error
+	}
+
+	err := backoff.Retry(operation, backoff.NewExponentialBackOff())
+	if err != nil {
+		// Handle error.
+		return
+	}
+
+	// Operation is successful.
+}
+
+func AvastRetry() {
+	retry.Attempts(3)
 }
